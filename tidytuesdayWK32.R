cea245007d228b41f2b3fd4fb56c798a7a0a0891
Mathew Varghese <mathewvarghese@mathews-mbp.lan>
2020-08-04 15:07:51 -0400

    Added comments.

--- tidytuesdayWK32.R
+++ tidytuesdayWK32.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(dplyr)
 library(tidyr)
 
-#loading the data
+#Loading the data
 tuesdata <- tidytuesdayR::tt_load('2020-08-04')
 
 #Cleaning the data
@@ -11,25 +11,27 @@ energy_types <- tuesdata$energy_types %>%
   filter(country_name != "NA")
 
 
-#Creating map data
-world_map <- map_data("world")
+#Creating map data and renaming region to country_name to make joins easier later
+world_map <- map_data("world") %>% 
+  rename(country_name = region)
 
+#Getting unique country names from the Tidy Tuesday dataset
 country_name <- unique(energy_types$country_name) 
 
+#Building the EU map by subestting the world map using EU country names
 eu_maps <- map_data("world", region = country_name)
 
-# Create Label coordinates for country names
+# Creating label coordinates for country names 
+# Original code for label creation: https://bit.ly/2DwIn8s
 country_names <- eu_maps %>%
   group_by(region) %>%
   summarise(long = mean(long), lat = mean(lat)) %>% 
   rename(country_name = region)
 
-#Original code for label creation: https://www.datanovia.com/en/blog/how-to-create-a-map-using-ggplot2/
-
-#Add Lat Long Coordinates to the energy data
+# Adding Lat Long coordinates to the energy data
 energy_latlong <- left_join(energy_primary,world_map_gp,by="country_name")
 
-#Plot the graph
+# Plotting the graph
 ggplot(energy_latlong, aes(x = long, y = lat))+
   geom_polygon(aes( group = group, fill = type))+
   geom_text(aes(label = country_name), data = country_names,size = 3, hjust = 0.5)+
@@ -43,6 +45,6 @@ ggplot(energy_latlong, aes(x = long, y = lat))+
     ),
     caption = "Data from https://bit.ly/3a0Pjac"
   )+
-  guides(fill=guide_legend(title=NULL))+  
+  guides(fill=guide_legend(title=NULL))+                                
   scale_fill_brewer(labels=c("Thermal", "Nuclear", "Hydroelectric","Wind"),palette = "Spectral")+
   theme(legend.position="bottom")
