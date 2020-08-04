4ec154d4b47cb8eb93af0655d2c887034af0883b
Mathew Varghese <mathewvarghese@mathews-mbp.lan>
2020-08-04 01:48:17 -0400

    Minor text changes after final.

--- tidytuesdayWK32.R
+++ tidytuesdayWK32.R
@@ -1,38 +1,30 @@
 library(ggplot2)
-library(ggrepel)
-require(psych)
 library(tidyverse)
-library(lubridate)
 library(dplyr)
-library(formattable)
 library(tidyr)
-library(readr)
 
 #loading the data
 tuesdata <- tidytuesdayR::tt_load('2020-08-04')
 
-#Creating the datasets
+#Cleaning the data
 energy_types <- tuesdata$energy_types %>% 
   filter(country_name != "NA")
 
-country_tots <- tuesdata$country_totals
 
-#Map View
+#Creating map data
 world_map <- map_data("world")
 
 country_name <- unique(energy_types$country_name) 
 
-
-
 eu_maps <- map_data("world", region = country_name)
 
-# Label coordinates for country names
+# Create Label coordinates for country names
 country_names <- eu_maps %>%
   group_by(region) %>%
   summarise(long = mean(long), lat = mean(lat)) %>% 
   rename(country_name = region)
 
-#https://www.datanovia.com/en/blog/how-to-create-a-map-using-ggplot2/
+#Original code for label creation: https://www.datanovia.com/en/blog/how-to-create-a-map-using-ggplot2/
 
 #Add Lat Long Coordinates to the energy data
 energy_latlong <- left_join(energy_primary,world_map_gp,by="country_name")
@@ -44,5 +36,5 @@ ggplot(energy_latlong, aes(x = long, y = lat))+
   theme_minimal()+
   ggtitle("How European Countries Predominantly Generated Electricity in 2018")+
   guides(fill=guide_legend(title=NULL))+  
-  scale_fill_discrete(labels=c("Thermal", "Hydroelectric", "Nuclear","Wind"))+
+  scale_fill_brewer(labels=c("Thermal", "Nuclear", "Hydroelectric","Wind"),palette = "Spectral")+
   theme(legend.position="bottom")
