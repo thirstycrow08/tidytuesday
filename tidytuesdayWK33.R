f8673371c27aefa59a72d44799e22fc0ce2c1733
thirstycrow08 <mathew.varghese@thirsty-crow.com>
2020-08-10 22:38:51 -0400

    Starting WK 33 project

---
+++ tidytuesdayWK33.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+library(ggrepel)
+require(psych)
+library(tidyverse)
+library(lubridate)
+library(dplyr)
+library(formattable)
+library(tidyr)
+library(readr)
+
+#loading the data
+tuesdata <- tidytuesdayR::tt_load('2020-08-11')
